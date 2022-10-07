50c3e0f3e20eb7c24bbab8df64973e6cb3da0407
vattanac <vattanacsim99@gmail.com>
2022-10-07 12:09:45 +0700

    17.Validating Parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
     <h1>A single user , with userID: {{ this.$route.params.id }}</h1>
 </template>
+
+<script>
+    export default {
+        validate(data) {
+            return /^\d+$/.test(data.params.id)
+        }
+    }
+</script>
